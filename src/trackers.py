2cb9556947a917cf47c4b2374c55fed2ce4742db
Jacob Lindbäck <jlindbac@kth.se>
2021-01-03 13:36:06 +0100

    First draft of the multi channel mosse on handmade features

--- src/trackers.py
+++ src/trackers.py
@@ -1,9 +1,9 @@
 import ncc
 import mosse
-
+import multi_channel_mosse
 
 def add_tracker_args(argument_parser):
-    argument_parser.add_argument("--tracker", choices=("NCC", "MOSSE"), required=True)
+    argument_parser.add_argument("--tracker", choices=("NCC", "MOSSE", "MCM"), required=True)
     ncc.add_cli_arguments(argument_parser)
     mosse.add_cli_arguments(argument_parser)
 
@@ -13,6 +13,8 @@ def get_tracker(args):
         tracker = ncc.get_tracker(args)
     elif args.tracker == "MOSSE":
         tracker = mosse.get_tracker(args)
+    elif args.tracker == "MCM":
+        tracker = multi_channel_mosse.get_multichannel_tracker(args)
     else:
         raise RuntimeError("Invalid tracker")
 
